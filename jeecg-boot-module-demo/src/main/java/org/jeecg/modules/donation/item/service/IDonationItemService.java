ec8eff6a82d87f275fd06d69a541773ae40bc131
SikeX <yoloxz719@gmail.com>
2021-10-21 18:34:39 +0800

    first commit

---
+++ jeecg-boot-module-demo/src/main/java/org/jeecg/modules/donation/item/service/IDonationItemService.java
@@ -0,0 +1,14 @@
+package org.jeecg.modules.donation.item.service;
+
+import org.jeecg.modules.donation.item.entity.DonationItem;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Description: 捐赠项目
+ * @Author: jeecg-boot
+ * @Date:   2021-10-21
+ * @Version: V1.0
+ */
+public interface IDonationItemService extends IService<DonationItem> {
+
+}
