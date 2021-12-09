11772a91618093d9791008ffbda2b949d98165ac
WangQingyuwqy <1369272088@qq.com>
2021-12-09 17:48:32 +0800

    commit一下

---
+++ jeecg-boot-module-demo/src/main/java/org/jeecg/modules/donation/item/mapper/DonationItemMapper.java
@@ -0,0 +1,17 @@
+package org.jeecg.modules.donation.item.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.jeecg.modules.donation.item.entity.DonationItem;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: 捐赠项目
+ * @Author: jeecg-boot
+ * @Date:   2021-10-21
+ * @Version: V1.0
+ */
+public interface DonationItemMapper extends BaseMapper<DonationItem> {
+
+}
---
+++ jeecg-boot-module-demo/src/main/java/org/jeecg/modules/donation/item/service/impl/DonationItemServiceImpl.java
@@ -0,0 +1,19 @@
+package org.jeecg.modules.donation.item.service.impl;
+
+import org.jeecg.modules.donation.item.entity.DonationItem;
+import org.jeecg.modules.donation.item.mapper.DonationItemMapper;
+import org.jeecg.modules.donation.item.service.IDonationItemService;
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+/**
+ * @Description: 捐赠项目
+ * @Author: jeecg-boot
+ * @Date:   2021-10-21
+ * @Version: V1.0
+ */
+@Service
+public class DonationItemServiceImpl extends ServiceImpl<DonationItemMapper, DonationItem> implements IDonationItemService {
+
+}
